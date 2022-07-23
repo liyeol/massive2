bccf600cf1030a574d686671aa9e909cd9731cd0
liyeol <krunkkatalea@gmail.com>
2022-07-23 17:25:17 +0100

    ver1

---
+++ ConsoleApplication1/bubblesort.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+using namespace std;
+
+class BubbleSort {
+public:
+	void bubblesort(int Array[], int x) {
+		int t;
+		for (int y = 0; y < x; y++)
+			for (int i = 0; i < x - y - 1; i++)
+				if (Array[i] > Array[i + 1]) {
+					t = Array[i];
+					Array[i] = Array[i + 1];
+					Array[i + 1] = t;
+				}
+	}
+};
+
+void Input_bs(int Array[], int x) {
+	cout << "Enter numbers: " << endl;
+	for (int i = 0; i <= x - 1; i++) {
+		cin >> Array[i];
+	}
+	cout << "Elements of unsorted list:  ";
+	for (int i = 0; i <= x - 1; i++) {
+		cout << Array[i] << " ";
+	}
+	cout << endl;
+
+}
+
+void Print_bs(int Array[], int x) {
+	cout << "Elements of sorted list:  ";
+	for (int i = 0; i < x; i++) {
+		cout << Array[i] << " ";
+	}
+}
+
+int main() {
+	int x;
+	cout << "Enter the count of element: ";
+	cin >> x;
+	int* Arr = new int[x];
+	BubbleSort A;
+	Input_bs(Arr, x);
+	A.bubblesort(Arr, x);
+	Print_bs(Arr, x);
+	return 0;
+}
+
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,61 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "C:\Users\Елизавета\source\repos\UnitTest1\ConsoleApplication1\bubblesort.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+
+		TEST_METHOD(InputTestMethod1)
+		{
+			BubbleSort A;
+			int i;
+			int Array[] = { 5, 4, 5, 2, 9 };
+			int size = sizeof(Array);
+			Input_bs(Array, 5);
+			Assert::IsTrue(5 == size);
+		}
+	};
+
+
+	TEST_CLASS(UnitTest2)
+	{
+	public:
+
+		TEST_METHOD(InputTestMethod2)
+		{
+			BubbleSort A;
+			int Array[] = { 5, 4, 5, 2, 9 };
+			Input_bs(Array, 5);
+			int x = Array[0];
+			Assert::IsTrue(1 == x);
+		}
+	};
+
+
+	TEST_CLASS(UnitTest3)
+	{
+	public:
+
+		TEST_METHOD(SortTestMethod3)
+		{
+			BubbleSort A;
+			const unsigned int count = 5;
+			int Array[count] = { 3, 6, 1, 7, 4 };
+			int SortArray[count] = { 1, 3, 4, 6, 7 };
+
+			A.bubblesort(Array, count);
+			for (int i = 0; i < count; i++)
+			{
+				Assert::AreEqual(SortArray[i], Array[i]);
+			}
+
+		};
+	};
+
+
+}
